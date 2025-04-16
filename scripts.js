1b0e3359ce899a66cfd1608d4312a2d42585a387
Laaarsyyy <listeriolars13@gmail.com>
2025-04-16 16:02:35 +0800

    sliders update

--- scripts.js
+++ scripts.js
@@ -1,6 +1,4 @@
-/*==============================Scroll Sections Active Link=========================== */
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+/*==============================Light mode=========================== */
 let lightmode = localStorage.getItem('lightmode');
 const themeSwitch = document.getElementById('theme-switch');
 
@@ -21,6 +19,11 @@ themeSwitch.addEventListener("click", () => {
     lightmode !== "active" ? enableLightmode() : disableLightmode()
 })
 
+
+/*==============================Navigation=========================== */
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
@@ -37,8 +40,6 @@ window.onscroll = () => {
     });
 };
 
-
-/*==============================Image/Text Animation=========================== */
 ScrollReveal({
     reset: true,
     distance: '80px',
@@ -47,7 +48,7 @@ ScrollReveal({
 });
 
 ScrollReveal().reveal('.home-content, .heading', {origin: 'top' });
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', {origin: 'bottom' });
+ScrollReveal().reveal('.home-img, .slider, .portfolio-box, .contact form', {origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', {origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right' });
 
@@ -59,3 +60,43 @@ const typed = new Typed('.multiple-text', {
     backDelay: 1000,
     loop: true
 });
+
+/*==============================Card Slider=========================== */
+
+let items = document.querySelectorAll('.slider .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+
+let active = 0;
+function loadShow(){
+    let stt = 0;
+    items[active].style.transform = 'none';
+    items[active].style.zIndex = 1;
+    items[active].style.filter = 'none';
+    items[active].style.opacity = 1;
+    for(var i = active + 1; i < items.length; i++){
+        stt++;
+        items[i].style.transform = `translateX(${120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(-1deg)`;
+        items[i].style.zIndex = -stt;
+        items[i].style.filter = 'blur(5px)';
+        items[i].style.opacity = stt > 2 ? 0 : 0.6;
+    }
+    stt = 0;
+    for(var i = active - 1; i >= 0; i--){
+        stt++;
+        items[i].style.transform = `translateX(${-120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(-1deg)`;
+        items[i].style.zIndex = -stt;
+        items[i].style.filter = 'blur(5px)';
+        items[i].style.opacity = stt > 2 ? 0 : 0.6;
+    }
+}
+
+loadShow();
+next.onclick = function(){
+    active = active + 1 < items.length ? active + 1 : active;
+    loadShow();
+}
+prev.onclick = function(){
+    active = active - 1 >= 0 ? active - 1 : active;
+    loadShow();
+}
